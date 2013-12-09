f1de664a521dab7b989f0f0f5e7b9447c5a51799
timey <timhartl89@gmail.com>
2013-12-09 18:29:26 +0100

    Landingpage überarbeitet
    - Aufteilung in versch. Container
    - Dropshadow
    - responsiveness angefasst

--- landingpage.php
+++ landingpage.php
@@ -2,30 +2,33 @@
 
 <?php get_header(); ?>
  
-<div class="row">
- <div class="bg large-12 columns">
+<!-- <div class="row">
+ <div class="bg large-12 columns first"> -->
 
 <!-- Über Uns Bereich -->
 
-  <div class="row first-container">
+  <div class="row first-container bg">
     <div class="large-12 columns">
       <div class="row">
-        <div class="large-6 large-offset-1 medium-6 small-12 columns">
+        <div class="large-7 medium-6 small-12 columns ueber-uns">
           <?php while(have_posts()):the_post();?>
           <h4><?php the_field('titel_box_ueber_uns'); ?></h4>
           <p><?php the_field('ueber_uns'); ?></p>
         <?php endwhile; ?>
         </div>
-         <div class="large-4 medium-6 small-12 columns">
+         <div class="large-5 medium-6 small-12 columns">
           <img class="avatar" src='<?php bloginfo('template_url'); ?>/img/borisch.jpg' alt="Harald Borisch"/>
         </div>
-         <div class="large-1 columns">
-        </div>
+        
       </div>
     </div>
   </div>
 
+
 <!-- Was wir bieten Trenner -->
+
+<div class="row">
+ <div class="bg large-12 columns">
   
 <div class="row second-container">
     <div class="large-3 large-centered  columns">
