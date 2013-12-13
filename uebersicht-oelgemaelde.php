7af68ba845ef374156d4f05d95732e507d86692a
sonjaka <sonja-karl@web.de>
2013-12-13 11:58:16 +0100

    -Ölgemälde Übersicht Filter added
    -Filter added
    -Not working properly yet

--- uebersicht-oelgemaelde.php
+++ uebersicht-oelgemaelde.php
@@ -5,66 +5,55 @@
  <div class="row">
  <div class="bg large-12 columns">
 
-<!--  <div class="row first-container">
-    <div class="large-12 columns">
-      <div class="row">
-
-        <?php query_posts('showposts=21'); ?>
-          <?php while (have_posts()) : the_post(); ?>
-          <div class="large-4 medium-4 columns imagefeed">
-           <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('index-categories'); ?></a>
-          </div>
-          <?php endwhile;?> 
-      <div class="clearfix">
-      </div>
-    </div>
-  </div>
-  </div> -->
-
 <!-- Loop der letzten 21 Post aus dem Post Type albertis-kunstwerke  -->
   <div class="row first-container">
     <div class="large-12 columns">
+
       <div class="row">
+  
+        <?php
+            // the query
 
-  <?php
-      // the query
-  $the_query = new WP_Query( array ( 
-                                    'post_type' => 'albertis-kunstwerke', 
-                                    'posts_per_page' => '21', 
-                                    'kunstarten' => 'oelgemaelde' ) ); ?> 
+         $args=array();
+         $args['post_type'] = 'albertis-kunstwerke';
+         $args['posts_per_page'] = '21';
+         $args['kunstarten'] = 'oelgemaelde';
 
+         $page_link = get_permalink();
+          $motive = get_terms( 'motive' );
 
+          $args['paged'] = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+         
+          if ( isset( $_GET['motive'] ) ) {
+              $args['motive'] = $_GET['motive'];
+              $page_link = add_query_arg( 'motive', $_GET['motive'], $page_link );
+          }
+
+          if ( is_array( $motive ) ) {
+              echo '<div class="filter_dropdown_wrapper large-4 medium-4 columns">';
+              echo '<a href=""><button>Filtern Sie hier nach Bildmotiv</button>';
+              echo '<ul class="filter_dropdown_main large-12 medium-12 columns">';
+              foreach ( $motive as $motive ){
+                  echo '<li class="filter_dropdown_tag"><a href="' . htmlentities( add_query_arg( 'motive', $motive->slug, $page_link ) ) . '">' . $motive->name . '</a></li>';
+              }
+              echo '<li class="filter_dropdown_tag"><a href="' . htmlentities( remove_query_arg( 'motive', $page_link ) ) . '">Keinen Filter anwenden</a></li>';
+              echo '</ul>';
+              echo '</a>';
+              echo '</div>';
+            } ;?>
+      </div>
+
+  <div class="row">
+
+<?php 
+  $the_query = new WP_Query( $args ); ?> 
 
       <?php if ( $the_query->have_posts() ) : ?>
 
         <!-- pagination here -->
-<!-- ALTER QUERY 
-        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-          <div class="large-4 medium-4 columns imagefeed">
-          <a href="<?php the_permalink(); ?>">
-            <?php the_post_thumbnail('index-categories'); ?>
-          
-            Pull Title and Artist
-
-            <div class="category_information columns absolute_center">
-              <h3 class="absolute_center">
-                <?php 
-                $post_id=$post->ID;
-                $post_title_custom=get_post_meta($post_id, 'bildname', true); 
-                echo $post_title_custom;
-                ?>
-              </h3>
-            </div>
-
-          </a>
-
-          </div> 
-        <?php endwhile; ?>-->
-
-<!-- 2. Version des Loops -->
-
         <!-- the loop -->
         <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
           <div class="large-4 medium-4 columns imagefeed kunstwerke">
           <a href="<?php the_permalink(); ?>">
             <?php the_post_thumbnail('index-categories'); ?>
