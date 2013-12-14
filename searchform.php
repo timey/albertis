861093a70f14161cc1b7e800c0ca5ee7d76f6a41
timey <timhartl89@gmail.com>
2013-12-14 01:20:02 +0100

    searchform angepasst
    - zeigt jetzt nach raus klicken wieder default wert an

--- searchform.php
+++ searchform.php
@@ -3,9 +3,6 @@
   	<!--BEGIN #searchform-->
  
 <form class="searchform" method="get" action="<?php bloginfo( 'url' ); ?>">
-	<input class="search" name="s" onclick="this.value=''" type="text" value="Suchen Sie hier!" tabindex="1" />
+	<input class="search" name="s" onclick="this.value=''" type="text" value="Suche..." onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;" tabindex="1" />
 <!--END #searchform-->
 </form>
-  
- 
-
